7acb8727224028ba5af1524f12d586fe197a1f02
tejuypatil <tejuypatil@gmail.com>
2022-07-15 23:03:45 +0530

    Added daily employee wage

--- src/com/bridglabz/employee/EmpWageComputation.java
+++ src/com/bridglabz/employee/EmpWageComputation.java
@@ -4,6 +4,7 @@ import static java.lang.Math.random;
 
 public class EmpWageComputation {
 
+<<<<<<< HEAD
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation");
@@ -14,4 +15,24 @@ public class EmpWageComputation {
             System.out.println("Employee is absent");
         }
     }
+=======
+    final static int WAGE_PER_HOUR = 20;
+    final static int FULL_Day_HOUR = 8;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+
+        int dailyEmpWage=0;
+        int randomCheck = (int) Math.round(random());
+
+        if (1 == randomCheck) {
+            System.out.println("Employee is present");
+            dailyEmpWage = WAGE_PER_HOUR * FULL_Day_HOUR;
+        } else {
+            System.out.println("Employee is absent");
+        }
+        System.out.println("Employee wage per day is :" + dailyEmpWage);
+    }
+    
+>>>>>>> a75afd8 (Added daily employee wage)
 }
